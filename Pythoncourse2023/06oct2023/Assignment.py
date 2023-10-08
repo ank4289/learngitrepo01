9f16705fb5e24071471414e1431b6a69f8723b3d
ank4289 <87062687+ank4289@users.noreply.github.com>
2023-10-08 17:21:40 +0530

    adding assigment

---
+++ Pythoncourse2023/06oct2023/Assignment.py
@@ -0,0 +1,20 @@
+input = input("Please enter integer to get the table")
+print(int(input))
+
+#Task 2:-----
+
+print(f"{input}*1 ={int(input)*1}")
+print(f"{input}*2 ={int(input)*2}")
+print(f"{input}*3 ={int(input)*3}")
+print(f"{input}*4 ={int(input)*4}")
+print(f"{input}*5 ={int(input)*5}")
+print(f"{input}*6 ={int(input)*6}")
+print(f"{input}*7 ={int(input)*7}")
+print(f"{input}*8 ={int(input)*8}")
+print(f"{input}*9 ={int(input)*9}")
+print(f"{input}*10 ={int(input)*10}")
+
+
+#Task 3:
+print("---------------------------------")
+print(dir(str))
